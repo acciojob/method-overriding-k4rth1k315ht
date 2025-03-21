86221d3c35587e88b3237e4cf368c14cc5d0abf2
k4rth1k315ht <karthikbisht506@gmail.com>
2025-03-21 22:35:22 +0530

    after long time again

--- src/main/java/com/driver/A.java
+++ src/main/java/com/driver/A.java
@@ -1,9 +1,2 @@
 package com.driver;
 
-public class A{
-    public String meth()
-    {
-        return "Invoking method from class A" ;
-
-    }
-}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,11 +2,29 @@ package com.driver;
 
 public class Main {
 
+    public class A{
+        public String meth()
+        {
+            return "Invoking method from class A" ;
+
+        }
+    }
+
+
+    public class B extends A {
+        @Override
+        public String meth()
+        {
+            return "Method is overridden in Extended class B";
+        }
+    }
+
    public static void main(String[] args)
    {
+       Main m = new Main();
 
-      B obj= new B();
-      obj.meth();       // returning class A string
+      B obj= m.new B();
+      obj.meth();       // returning class A method string but after creating class b method it will return its method string
 
       obj.meth();
 
